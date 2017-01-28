7a213cef8b4f9ce94bda32ea1e4e7c92dce64cea
ccook7 <ccook7@stu.jsu.edu>
2017-01-28 16:59:00 -0600

    Gui Test added

--- tic-tac-toe-project/src/main/java/edu/jsu/mcis/TicTacToeGui.java
+++ tic-tac-toe-project/src/main/java/edu/jsu/mcis/TicTacToeGui.java
@@ -1,6 +1,19 @@
 package edu.jsu.mcis;
 
 public class TicTacToeGui extends JPanal implements ActionListener {
-
+	
+	private TicTacToe game;
+	private JButton[][] grid;
+	
+	public TicTacToeGui() {
+		game = new TicTacToe();
+		grid = new JButton[3][3];
+		for(int i = 0; i < 0; i++){
+			for(int j = 0; j < 0; j++){
+				
+			}
+		}
+	}
+	
 
 }
---
+++ tic-tac-toe-project/src/test/java/edu/jsu/mcis/TicTacToeGuiTest.java
@@ -0,0 +1,54 @@
+package edu.jsu.mcis;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TicTacToeGuiTest {
+	private TicTacToe game;
+	@Before
+	public final void setUp() {
+		game = new TicTacToe();
+	}
+	
+	@Test
+	public void testInitialBoardIsEmpty(){
+		
+	}
+	
+	@Test
+	public final void testButtonsAreNamedCorrectly(){		
+	
+	}
+	
+	@Test
+	public void testMarkXInUpperRightCorner() {
+		
+	}
+	
+	@Test
+	public void testMarkOInBottomLeftCorner() {
+		
+	}
+	
+	@Test
+	public void testUnableToMarkOverExistingMark() {
+		
+	}
+	
+	@Test
+	public void testGameIsNotOverAfterTheFirstMark() {
+		
+		
+	}
+	
+	@Test
+	public void testGameIsWonByXHorizontallyAcrossTopRow() {
+		
+		
+	}
+	
+	@Test
+	public void testGameIsOverByTieIfAllLocationsAreFilled() {
+		
+	}
+}
